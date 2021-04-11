fe383723b7bde9a76c7bb419ea582b365dfe955a
jo95holland <jo95holland@gmail.com>
2021-04-11 23:04:55 +0200

    Example images should now display the correct predictions by the discriminator

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ def main():
 if __name__ == "__main__":
     physical_devices = tf.config.list_physical_devices("GPU")
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    tf.config.run_functions_eagerly(True)
 
     noise_dim = 10
     image_width = 150
--- train.py
+++ train.py
@@ -6,15 +6,15 @@ import time
 import matplotlib.pyplot as plt
 
 
-def discriminator_loss(cross_entropy, real_output, gan_output):
+def discriminator_loss(cross_entropy, real_output, gen_output):
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
-    fake_loss = cross_entropy(tf.zeros_like(gan_output), gan_output)
-    total_loss = real_loss + fake_loss
+    gen_loss = cross_entropy(tf.zeros_like(gen_output), gen_output)
+    total_loss = real_loss + gen_loss
     return total_loss
 
 
-def generator_loss(cross_entropy, gan_output):
-    return cross_entropy(tf.ones_like(gan_output), gan_output)
+def generator_loss(cross_entropy, gen_output):
+    return cross_entropy(tf.ones_like(gen_output), gen_output)
 
 
 @tf.function
@@ -23,10 +23,10 @@ def train_step(generator, discriminator, images, cross_entropy, g_optimizer, d_o
         generated_image = generator(tf.image.rgb_to_grayscale(images), training=True)
 
         real_output = discriminator(images, training=True)
-        gan_output = discriminator(generated_image, training=True)
+        gen_output = discriminator(generated_image, training=True)
 
-        gen_loss = generator_loss(cross_entropy, gan_output)
-        disc_loss = discriminator_loss(cross_entropy, real_output, gan_output)
+        gen_loss = generator_loss(cross_entropy, gen_output)
+        disc_loss = discriminator_loss(cross_entropy, real_output, gen_output)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
@@ -43,15 +43,18 @@ def train(generator, discriminator, dataset, cross_entropy, g_optimizer, d_optim
             train_step(generator, discriminator, image_batch, cross_entropy, g_optimizer, d_optimizer)
 
         print("Time for epoch {} is {} sec".format(epoch + 1, time.time() - start))
-        convert_random(generator)
+        convert_random(discriminator, generator)
 
 
-def convert_random(generator):
+def convert_random(discriminator, generator):
     filename = random.choice(os.listdir("data/seg_train/forest/sub"))
     path = "data/seg_train/forest/sub/" + filename
 
     rgb_image = tf.keras.preprocessing.image.load_img(path, target_size=(150, 150))
     rgb_image_tensor = tf.keras.preprocessing.image.img_to_array(rgb_image)
+    disc_result = discriminator.predict(tf.expand_dims(rgb_image_tensor, axis=0))
+    plt.text(55, -10, str(round(disc_result[0, 0] * 100, 1)) + "%", fontsize=20)
+
     plt.imshow(rgb_image_tensor/255)
     plt.axis("off")
     plt.show()
@@ -59,6 +62,10 @@ def convert_random(generator):
     grayscale_image = tf.image.rgb_to_grayscale(rgb_image_tensor)
     grayscale_image = tf.expand_dims(grayscale_image, axis=0)
     gan_image = generator(grayscale_image, training=False)
+    disc_result = discriminator.predict(gan_image)
+    print(disc_result)
+
+    plt.text(55, -10, str(round(disc_result[0, 0] * 100, 1)) + "%", fontsize=20)
 
     plt.imshow(gan_image[0, :, :, 0] * 127.5 + 127.5)
     plt.axis("off")
