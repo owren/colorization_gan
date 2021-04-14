b03a8329968071e80e38829be9599f71b02643dc
jo95holland <jo95holland@gmail.com>
2021-04-14 17:10:15 +0200

    Small adjustments

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ HEIGHT = 128
 BATCH_SIZE = 32
 
 learning_rate = 2e-4
-g_optimizer = tf.keras.optimizers.Adam(learning_rate)
-d_optimizer = tf.keras.optimizers.Adam(learning_rate)
+g_optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.5)
+d_optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.5)
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
