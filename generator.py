855a22010e4fabdc8d9051cb67ff236bfa9080f3
Jon-Olav Holland <jonolh17@uia.no>
2021-05-05 16:42:29 +0200

    Stride 1 and skip connections

--- generator.py
+++ generator.py
@@ -6,43 +6,36 @@ from config import *
 def create_generator():
     inp = tf.keras.layers.Input(shape=[HEIGHT, WIDTH, 1])
 
-    down_stack = [
-        downsample(64, (4, 4), (2, 2), batchnorm=False),
-        downsample(128, (4, 4), (2, 2)),
-        downsample(256, (4, 4), (2, 2)),
-        downsample(512, (4, 4), (2, 2)),
-        downsample(512, (4, 4), (2, 2)),
-        downsample(512, (4, 4), (2, 2)),
-        downsample(512, (4, 4), (2, 2)),
-    ]
-
-    up_stack = [
-        upsample(512, (4, 4), (2, 2), dropout=True),
-        upsample(512, (4, 4), (2, 2), dropout=True),
-        upsample(512, (4, 4), (2, 2)),
-        upsample(256, (4, 4), (2, 2)),
-        upsample(128, (4, 4), (2, 2)),
-        upsample(64, (4, 4), (2, 2))
+    stack = [
+        downsample(64, (3, 3), (1, 1), batchnorm=False),
+        downsample(128, (3, 3), (1, 1)),
+        downsample(256, (3, 3), (1, 1)),
+        downsample(512, (3, 3), (1, 1)),
+        downsample(512, (3, 3), (1, 1)),
+        downsample(512, (3, 3), (1, 1)),
+        downsample(512, (3, 3), (1, 1)),
+        downsample(512, (3, 3), (1, 1)),
+        downsample(1024, (3, 3), (1, 1))
     ]
 
     output = tf.keras.layers.Conv2DTranspose(2,
                                              kernel_size=(4, 4),
-                                             strides=(2, 2),
+                                             strides=(1, 1),
                                              padding="same",
                                              use_bias=False,
                                              activation="tanh")
 
     x = inp
     skips = []
-    for down in down_stack:
-        x = down(x)
-        skips.append(x)
 
-    skips = reversed(skips[:-1])
+    for i in range(len(stack) - 2):
+        next_layer = stack[i](x)
 
-    for up, skip in zip(up_stack, skips):
-        x = up(x)
-        x = tf.keras.layers.Concatenate()([x, skip])
+        if i % 2 == 0:
+            conc = tf.keras.layers.Concatenate()([x, next_layer])
+            x = conc
+        else:
+            x = next_layer
 
     x = output(x)
 
@@ -50,5 +43,6 @@ def create_generator():
 
     # Uncomment to visualize the model
     # tf.keras.utils.plot_model(model, to_file="gen.png", show_shapes=True, dpi=64)
+    # model.summary()
 
     return model
